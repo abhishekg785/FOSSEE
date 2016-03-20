50cecbab63118ee3c6c136e34f1c64cebbb94509
ABHISHEK GSWAMI <abhishekg785@gmail.com>
2016-03-20 16:16:50 +0530

    models UserInfo created and migrations were made

--- projDir/app/models.py
+++ projDir/app/models.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class UserInfo(models.Model):
+    fname = models.CharField(max_length = 200)
+    lname = models.CharField(max_length = 200)
+    username = models.CharField(max_length = 200)
+    password = models.CharField(max_length = 200)
+
+
