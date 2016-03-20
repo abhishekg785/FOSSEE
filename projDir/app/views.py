be3dfdbdf8af2fa41a099ad2bbc5331108803b53
ABHISHEK GSWAMI <abhishekg785@gmail.com>
2016-03-20 21:46:02 +0530

    view demo checked and working

--- projDir/app/views.py
+++ projDir/app/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
+from .models import UserInfo
 # Create your views here.
 def index(request):
+    print (request.method)
     return render(request,'index.html')
 
-def register(get):
-    return render(get,'register.html')
+def register(request):
+    print request.method
+    if request.method == 'GET':
+        return render(request,'register.html')
+   # print request.POST['fname']
+    new_user = UserInfo(fname = request.POST['fname'])
+    new_user.save()
+    return render(request,'index.html')    
+
 
