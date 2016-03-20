84940b5376715fda967bdc8003985544fc517461
ABHISHEK GSWAMI <abhishekg785@gmail.com>
2016-03-20 17:06:37 +0530

    template and url for registering user created

--- projDir/projDir/urls.py
+++ projDir/projDir/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from app.views import index
+from app.views import index,register
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$',index),
+    url(r'^register/',register)
 ]
